c9a1d485a89a4117dc3d553eff5146074e038b19
mostafa khedr <mkhedr@etmam.tech>
2019-07-24 02:00:35 +0200

    add admin

--- app/src/main/java/com/ddg/sgah/ui/activity/UploadActivity.java
+++ app/src/main/java/com/ddg/sgah/ui/activity/UploadActivity.java
@@ -1,5 +1,6 @@
 package com.ddg.sgah.ui.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,7 +16,8 @@ public class UploadActivity extends AppCompatActivity {
     }
 
     public void add(View view) {
-
+        startActivity(new Intent(UploadActivity.this, AdminActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+        finish();
 
     }
 }
